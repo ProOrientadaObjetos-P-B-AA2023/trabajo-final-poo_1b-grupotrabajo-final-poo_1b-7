aa15878fbaa9c870b973a1c956a8f86694f0cf2a
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-06-04 23:22:59 -0500

    Update Constructora.java

--- AdministrarInmobiliaria/src/paquete05/Constructora.java
+++ AdministrarInmobiliaria/src/paquete05/Constructora.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete05;
 
-/**
- *
- * @author reroes
- */
-public class Constructora {
-    
+import java.io.Serializable;
+
+public class Constructora implements Serializable{
+    private String nombreConstructora;
+    private String idEmpresa;
+
+    public Constructora(){
+
+    }
+    public Constructora(String nombreConstructora, String idEmpresa) {
+        this.nombreConstructora = nombreConstructora;
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombreConstructora() {
+        return nombreConstructora;
+    }
+
+    public void setNombreConstructora(String nombreConstructora) {
+        this.nombreConstructora = nombreConstructora;
+    }
+
+    public String getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(String idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
 }
