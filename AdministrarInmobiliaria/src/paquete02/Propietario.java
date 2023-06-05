f3cda3422497d03ec42d23fa642ead4dba697ac4
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-06-04 23:21:35 -0500

    Update Propietario.java

--- AdministrarInmobiliaria/src/paquete02/Propietario.java
+++ AdministrarInmobiliaria/src/paquete02/Propietario.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete02;
 
-/**
- *
- * @author reroes
- */
-public class Propietario {
-    
+import java.io.*;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Propietario implements Serializable{
+    private String nombres;
+    private String apellidos;
+    private String identificacion;
+
+    public Propietario(){
+
+    }
+
+    public Propietario(String nombres, String apellidos, String identificacion) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.identificacion = identificacion;
+    }
+
+    public String getNombres() {
+
+        return nombres;
+    }
+
+    public String getApellidos() {
+
+        return apellidos;
+    }
+
+    public String getIdentificacion() {
+
+        return identificacion;
+    }
+
+    public void setNombres(String nombres) {
+
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+
+        this.apellidos = apellidos;
+    }
+
+    public void setIdentificacion(String identificacion) {
+
+        this.identificacion = identificacion;
+    }
 }
+
