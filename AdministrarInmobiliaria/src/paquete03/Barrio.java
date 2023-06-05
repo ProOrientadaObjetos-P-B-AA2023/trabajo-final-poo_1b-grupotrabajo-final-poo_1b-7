8fb5b9e6a83654b6c7d93c0b795b43d6ac101c63
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-06-04 23:22:05 -0500

    Update Barrio.java

--- AdministrarInmobiliaria/src/paquete03/Barrio.java
+++ AdministrarInmobiliaria/src/paquete03/Barrio.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete03;
 
-/**
- *
- * @author reroes
- */
-public class Barrio {
-    
+import java.io.Serializable;
+
+public class Barrio implements Serializable{
+    private String nombreBarrio;
+    private String referencia;
+
+    public Barrio(){
+
+    }
+
+    public Barrio(String nombreBarrio, String referencia) {
+        this.nombreBarrio = nombreBarrio;
+        this.referencia = referencia;
+    }
+
+    public String getNombreBarrio() {
+        return nombreBarrio;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setNombreBarrio(String nombreBarrio) {
+
+        this.nombreBarrio = nombreBarrio;
+    }
+
+    public void setReferencia(String referencia) {
+
+        this.referencia = referencia;
+    }
 }
